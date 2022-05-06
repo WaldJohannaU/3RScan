b287dfa54723535dbb38794bfb495f5ad052ad70
Johanna Wald <johanna.wald@tum.de>
2022-05-06 22:09:26 +0200

    Update webserver links in setup.sh

--- setup.sh
+++ setup.sh
@@ -7,20 +7,20 @@ fi
 # download example data
 if [[ ! -d "data/3RScan" ]]; then
 	if [[ ! -f "data/3RScan.v2.zip" ]]; then
-		wget "http://campar.in.tum.de/files/3RScan/3RScan.v2.zip" -P data
+		wget "http://campar.in.tum.de/public_datasets/3RScan/3RScan.v2.zip" -P data
 	fi
 	unzip "data/3RScan.v2.zip" -d ./data/3RScan
 fi
 
 # download 3RScan metafile and 3DSSG data
 if [[ ! -f "data/3RScan/3RScan.json" ]]; then
-	wget "http://campar.in.tum.de/files/3RScan/3RScan.json" -P data/3RScan
+	wget "http://campar.in.tum.de/public_datasets/3RScan/3RScan.json" -P data/3RScan
 fi
 if [[ ! -f "data/3RScan/objects.json" ]]; then
-	wget "http://campar.in.tum.de/files/3DSSG/3DSSG/objects.json" -P data/3RScan
+	wget "http://campar.in.tum.de/public_datasets/3DSSG/3DSSG/objects.json" -P data/3RScan
 fi
 if [[ ! -f "data/3RScan/relationships.json" ]]; then
-	wget "http://campar.in.tum.de/files/3DSSG/3DSSG/relationships.json" -P data/3RScan
+	wget "http://campar.in.tum.de/public_datasets/3DSSG/3DSSG/relationships.json" -P data/3RScan
 fi
 
 echo "sucessfully setup data folder."
